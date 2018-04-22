ad7c1683bbcc284b8e31699d70de1e3a633c0535
Kevan <kevan1017@163.com>
2018-04-22 18:02:21 +0800

    DOM操作

--- script/showPic.js
+++ script/showPic.js
@@ -2,4 +2,13 @@ function showPic(whichPic){
   var source = whichPic.getAttribute("href");
   var placeholder = document.getElementById("placeholder");
   placeholder.setAttribute("src",source);
+  var text = whichPic.getAttribute("title");
+  var description = document.getElementById("description");
+  description.firstChild.nodeValue = text;
 }
+
+function countBodyChildren(){
+  var body_element = document.getElementsByTagName("body")[0];
+  console.log(body_element.nodeType);
+}
+countBodyChildren()
